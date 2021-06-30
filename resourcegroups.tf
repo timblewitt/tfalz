9dea751a27fc029aeedf5f246e05ff206a711f33
Tim Blewitt <timblewitt@hotmail.com>
2021-06-30 09:33:34 +0100

    Removed test text

--- resourcegroups.tf
+++ resourcegroups.tf
@@ -17,8 +17,3 @@ resource "azurerm_resource_group" "management" {
   name     = "rg-${var.org}-${var.region_id}-${var.env}-management"
   location = var.region_name
 }
-
-resource "azurerm_resource_group" "temp2" {
-  name     = "rg-${var.org}-${var.region_id}-${var.env}-temp2"
-  location = var.region_name
-}
